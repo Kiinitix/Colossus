ee83bd3a7872080592d3ca516f11ad674fb3663c
Kabir Dhruw <34811605+Kiinitix@users.noreply.github.com>
2022-02-04 16:51:17 +0530

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -5,17 +5,25 @@ from Crypto.Cipher import AES
 from Crypto import Random 
 import hybrid
 
-pri=input("Enter the Private Key: ")
-cipherKey=input("Enter the AES Symmetric Key: ")
-cipherText=input("Enter cipher text: ")
+def decryptAES(cipherAESd,cipherText):
+    dec= cipherAESd.decrypt(cipherText)
+    return dec
+
+def decrypt(pk, ciphertext):
+    d, n = pk
+    m = [chr((char ** d) % n) for char in ciphertext]
+    return m
+
+pri = tuple(int(item) for item in input("Enter the Private Key: ").split(','))
+cipherKey=[int(item) for item in input("Enter the AES Symmetric Key: ").split(',')]
+cipherText= <cipher-text-from-the-file>
+nonce= <included-in-email>
 
 decriptedKey=''.join(decrypt(pri,cipherKey))
-print()
 print("Decrypting the AES Symmetric Key...")
 
 decriptedKey=decriptedKey.encode('utf-8')
 cipherAESd = AES.new(decriptedKey, AES.MODE_GCM, nonce=nonce)
 decrypted=decryptAES(cipherAESd,cipherText)
-print()
-print("Decrypting the message using the AES symmetric key.....")
+print("\nDecrypting the message using the AES symmetric key.....")
 print("decrypted message: ", decrypted)
