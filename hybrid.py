e97b0d2e114cb99270085407d10611e4eb2a3347
Kabir Dhruw <34811605+Kiinitix@users.noreply.github.com>
2022-02-04 17:49:41 +0530

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -143,7 +143,8 @@ def main():
     print("Encrypting the message with AES......")
     cipherText=encryptAES(cipherAESe,plainText)
     f=open(r"<text-file>","w+b")
-    f.write(cipherText)
+    txt = str(cipherText).encode()
+    f.write(txt)
     f.close()
     print("Upload Done")
     #Encrypt the symmetric key under the key encapsulation scheme, using Alice’s public key.
